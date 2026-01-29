71e73678015fc16abbb20bfaf252bababece9d99
Roshangone <89498709+Roshangone@users.noreply.github.com>
2026-01-29 09:49:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ async function loadAsteroids() {
 // Load initial data for all sections
 window.onload = function() {
     // const today = new Date().toISOString().split("T")[0];
-    const today = new Date().toLocaleDateString('en-IN', { timeZone: 'America/New_York' });
+    const today = new Date().toLocaleDateString('en-CA', { timeZone: 'America/New_York' });
     apodDateInput.value = today;
     loadAPOD(today);
     loadRoverPhotos();
